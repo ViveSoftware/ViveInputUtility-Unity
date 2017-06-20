de880503767aee644b5710cdf96bd010f7851144
lawwong <lawrence_wong@htc.com>
2017-06-20 11:26:59 +0800

    Add SteamVR_Camera component before SteamVR_Render initialized

--- Assets/HTC.UnityPlugin/ViveInputUtility/Scripts/Misc/SteamVRCameraHook.cs
+++ Assets/HTC.UnityPlugin/ViveInputUtility/Scripts/Misc/SteamVRCameraHook.cs
@@ -1,47 +1,47 @@
 ﻿//========= Copyright 2016-2017, HTC Corporation. All rights reserved. ===========
 
-using UnityEngine;
-using System.Collections;
 using HTC.UnityPlugin.VRModuleManagement;
+using UnityEngine;
 
 public class SteamVRCameraHook : MonoBehaviour
 {
-    IEnumerator Start()
-    {
 #if VIU_STEAMVR
-        while (VRModule.activeModule == SupportedVRModule.Uninitialized)
+    private void Awake()
+    {
+        if (VRModule.activeModule == SupportedVRModule.Uninitialized)
         {
-            yield return null;
+            VRModule.onModuleActivatedEvent.AddListener(OnModuleActivated);
         }
-
-        if (VRModule.activeModule == SupportedVRModule.SteamVR)
+        else
         {
-            if (SteamVR_Render.Top() != null)
-            {
-                // SteamVR_Camera already exist in the scene
-            }
-            else
-            {
-                var earsComp = default(SteamVR_Ears);
-                for (int i = transform.childCount - 1; i >= 0; --i)
-                {
-                    earsComp = transform.GetChild(i).GetComponentInChildren<SteamVR_Ears>();
-                    if (earsComp != null) { break; }
-                }
-
-                if (earsComp == null)
-                {
-                    var ears = new GameObject("Camera (ears)", typeof(AudioListener), typeof(SteamVR_Ears));
-                    ears.transform.SetParent(transform, false);
-                }
-
-                if (GetComponent<SteamVR_Camera>() == null)
-                {
-                    gameObject.AddComponent<SteamVR_Camera>();
-                }
-            }
+            OnModuleActivated(VRModule.activeModule);
         }
-#endif
-        yield break;
     }
+
+    private void OnModuleActivated(SupportedVRModule activatedModule)
+    {
+        if (activatedModule == SupportedVRModule.SteamVR)
+        {
+            var earsComp = default(SteamVR_Ears);
+            for (int i = transform.childCount - 1; i >= 0; --i)
+            {
+                earsComp = transform.GetChild(i).GetComponentInChildren<SteamVR_Ears>();
+                if (earsComp != null) { break; }
+            }
+
+            if (earsComp == null)
+            {
+                var ears = new GameObject("Camera (ears)", typeof(AudioListener), typeof(SteamVR_Ears));
+                ears.transform.SetParent(transform, false);
+            }
+
+            if (GetComponent<SteamVR_Camera>() == null)
+            {
+                gameObject.AddComponent<SteamVR_Camera>();
+            }
+
+            VRModule.onModuleActivatedEvent.RemoveListener(OnModuleActivated);
+        }
+    }
+#endif
 }
