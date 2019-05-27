6d4ac91eca9b4df4b007860f01bccbe97d2c4cd4
chengnay <chengnan_yang@htc.com>
2019-05-27 16:40:24 +0800

    Fix null exception error message

--- Assets/HTC.UnityPlugin/ViveInputUtility/Scripts/Editor/VIUSettingsEditor.cs
+++ Assets/HTC.UnityPlugin/ViveInputUtility/Scripts/Editor/VIUSettingsEditor.cs
@@ -389,6 +389,8 @@ namespace HTC.UnityPlugin.Vive
             public static bool IsPackageInList(string name)
             {
                 Debug.Assert(m_listRequest != null);
+                if (m_listRequest.Result == null) return false;
+
                 return m_listRequest.Result.Any(pkg => pkg.name == name);
             }
 
