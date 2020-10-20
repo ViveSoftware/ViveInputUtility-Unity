a7dcdba69582ae4382db0d7c54927b713fa79852
Naga Chiang <nagachiangdev@gmail.com>
2020-10-20 17:32:10 +0800

    Add TouchSlider

---
+++ Assets/HTC.UnityPlugin/ViveInputUtility/Examples/8.NearFieldHandInteraction/Scripts/HoverEventHandler.cs
@@ -0,0 +1,103 @@
+﻿//========= Copyright 2016-2020, HTC Corporation. All rights reserved. ===========
+
+using System.Collections.Generic;
+using HTC.UnityPlugin.ColliderEvent;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Events;
+
+namespace HTC.UnityPlugin.Vive
+{
+    public class HoverEventHandler : MonoBehaviour, IColliderEventHoverEnterHandler, IColliderEventHoverExitHandler
+    {
+        public UnityEvent<ColliderHoverEventData> hoverEntered;
+        public UnityEvent<Transform> hoverStaying;
+        public UnityEvent<ColliderHoverEventData> hoverExited;
+
+        private Dictionary<ViveRoleProperty, int> m_stayingRoleCounts = new Dictionary<ViveRoleProperty, int>();
+        private Transform m_stayingTransform;
+
+        public void OnColliderEventHoverEnter(ColliderHoverEventData eventData)
+        {
+            ViveColliderEventCaster caster;
+            if (eventData.TryGetEventCaster(out caster))
+            {
+                int count;
+                if (m_stayingRoleCounts.TryGetValue(caster.viveRole, out count))
+                {
+                    m_stayingRoleCounts[caster.viveRole]++;
+                }
+                else
+                {
+                    m_stayingRoleCounts.Add(caster.viveRole, 1);
+                }
+
+                if (m_stayingRoleCounts[caster.viveRole] == 1)
+                {
+                    InvokeEnterEvent(eventData);
+                    m_stayingTransform = caster.transform;
+                }
+            }
+            else
+            {
+                InvokeEnterEvent(eventData);
+            }
+        }
+
+        public void OnColliderEventHoverExit(ColliderHoverEventData eventData)
+        {
+            ViveColliderEventCaster caster;
+            if (eventData.TryGetEventCaster(out caster))
+            {
+                Assert.IsTrue(m_stayingRoleCounts.ContainsKey(caster.viveRole), "Exiting role should have entered before.");
+
+                m_stayingRoleCounts[caster.viveRole]--;
+                if (m_stayingRoleCounts[caster.viveRole] == 0)
+                {
+                    InvokeExitEvent(eventData);
+                }
+
+                if (m_stayingTransform == caster.transform)
+                {
+                    m_stayingTransform = null;
+                }
+            }
+            else
+            {
+                InvokeExitEvent(eventData);
+            }
+        }
+
+        protected virtual void Update()
+        {
+            if (m_stayingTransform)
+            {
+                InvokeStayEvent(m_stayingTransform);
+            }
+        }
+
+        private void InvokeEnterEvent(ColliderHoverEventData eventData)
+        {
+            if (hoverEntered != null)
+            {
+                hoverEntered.Invoke(eventData);
+            }
+        }
+
+        private void InvokeStayEvent(Transform transform)
+        {
+            if (hoverEntered != null)
+            {
+                hoverStaying.Invoke(transform);
+            }
+        }
+
+        private void InvokeExitEvent(ColliderHoverEventData eventData)
+        {
+            if (hoverEntered != null)
+            {
+                hoverExited.Invoke(eventData);
+            }
+        }
+    }
+}
---
+++ Assets/HTC.UnityPlugin/ViveInputUtility/Examples/8.NearFieldHandInteraction/Scripts/TouchSlider.cs
@@ -0,0 +1,103 @@
+﻿//========= Copyright 2016-2020, HTC Corporation. All rights reserved. ===========
+
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Events;
+
+namespace HTC.UnityPlugin.Vive
+{
+    public class TouchSlider : MonoBehaviour
+    {
+        public UnityEvent<float> valueChanged;
+
+        [SerializeField] private Transform m_baseTransform;
+        [SerializeField] private Transform m_barTransform;
+        [SerializeField] private Vector2 m_barPadding;
+        [SerializeField] private float m_barHeightOffset = 0.001f;
+
+        [Range(0.0f, 1.0f)]
+        [SerializeField] private float m_value = 1.0f;
+
+        [SerializeField] private float m_maxLength;
+
+        public void SetValue(float value)
+        {
+            m_value = Mathf.Clamp01(value);
+            UpdateBarTransform();
+            InvokeValueChangeEvent();
+        }
+
+        public void OnHoverStaying(Transform trans)
+        {
+            if (!trans)
+            {
+                return;
+            }
+
+            Vector3 barStartPos = GetBarStartLocalPosition();
+            Vector3 collisionPos = m_baseTransform.parent.InverseTransformPoint(trans.position);
+            float value = Mathf.Clamp01((collisionPos.x - barStartPos.x) / GetBarLength());
+
+            SetValue(value);
+        }
+
+        protected virtual void Start()
+        {
+            Assert.IsNotNull(m_baseTransform);
+            Assert.IsNotNull(m_barTransform);
+            Assert.IsTrue(m_barPadding.x >= 0);
+            Assert.IsTrue(m_barPadding.y >= 0);
+        }
+
+        private void UpdateBarTransform()
+        {
+            Vector3 barScale = GetBarMaxScale();
+            float offset = barScale.x * (1.0f - m_value);
+            barScale.x -= offset;
+
+            Vector3 basePos = m_baseTransform.localPosition;
+            Vector3 barPos = new Vector3(basePos.x - (offset / 2.0f), basePos.y + m_barHeightOffset, basePos.z);
+
+            m_barTransform.localPosition = barPos;
+            m_barTransform.localScale = barScale;
+        }
+
+        private Vector3 GetBarMaxScale()
+        {
+            float x = m_baseTransform.localScale.x - (m_barPadding.x * 2);
+            float y = m_baseTransform.localScale.y;
+            float z = m_baseTransform.localScale.z - (m_barPadding.y * 2);
+
+            return new Vector3(x, y, z);
+        }
+
+        private Vector3 GetBarStartLocalPosition()
+        {
+            Vector3 basePos = m_baseTransform.localPosition;
+            float x = basePos.x - (m_baseTransform.localScale.x / 2.0f) + m_barPadding.x;
+
+            return new Vector3(x, basePos.y, basePos.z);
+        }
+
+        private float GetBarLength()
+        {
+            return m_baseTransform.localScale.x - (m_barPadding.x * 2);
+        }
+
+        private void InvokeValueChangeEvent()
+        {
+            if (valueChanged != null)
+            {
+                valueChanged.Invoke(m_value);
+            }
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            UpdateBarTransform();
+            InvokeValueChangeEvent();
+        }
+#endif
+    }
+}
