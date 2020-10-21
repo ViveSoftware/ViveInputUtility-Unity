f4d058e65dc1d31f370a244d6b469139f2a28f91
Naga Chiang <nagachiangdev@gmail.com>
2020-10-21 16:10:39 +0800

    Add grab and throw

---
+++ Assets/HTC.UnityPlugin/ViveInputUtility/Examples/8.NearFieldHandInteraction/Scripts/Ball.cs
@@ -0,0 +1,19 @@
+﻿//========= Copyright 2016-2020, HTC Corporation. All rights reserved. ===========
+
+using UnityEngine;
+
+namespace HTC.UnityPlugin.Vive
+{
+    public class Ball : MonoBehaviour
+    {
+        public void OnGrabbed()
+        {
+            Detach();
+        }
+
+        private void Detach()
+        {
+            transform.parent = null;
+        }
+    }
+}
---
+++ Assets/HTC.UnityPlugin/ViveInputUtility/Examples/8.NearFieldHandInteraction/Scripts/ItemSpawner.cs
@@ -0,0 +1,39 @@
+﻿//========= Copyright 2016-2020, HTC Corporation. All rights reserved. ===========
+
+using UnityEngine;
+
+namespace HTC.UnityPlugin.Vive
+{
+    public class ItemSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject m_itemPrefab;
+
+        private GameObject m_spawnedItem;
+
+        private void Awake()
+        {
+            Spawn();
+        }
+
+        private void OnTriggerExit(Collider collider)
+        {
+            Transform trans = m_spawnedItem.transform;
+            do
+            {
+                if (trans.gameObject == m_spawnedItem)
+                {
+                    Spawn();
+                    break;
+                }
+
+                trans = trans.parent;
+            } while (trans);
+        }
+
+        private void Spawn()
+        {
+            GameObject obj = Instantiate(m_itemPrefab, transform);
+            m_spawnedItem = obj;
+        }
+    }
+}
