811b87fff4173d4b5ba0624c77ae82062b5a2faf
Naga Chiang <nagachiangdev@gmail.com>
2020-11-16 16:06:46 +0800

    Fix TryGetHandJointPose() with unsupported joints

--- Assets/HTC.UnityPlugin/VRModule/VRModuleDeviceState.cs
+++ Assets/HTC.UnityPlugin/VRModule/VRModuleDeviceState.cs
@@ -311,13 +311,7 @@ namespace HTC.UnityPlugin.VRModuleManagement
 
             public bool TryGetHandJointPose(HandJointName jointName, out JointPose pose)
             {
-                if (m_handJoints == null)
-                {
-                    pose = default(JointPose);
-                    return false;
-                }
-
-                if (!m_handJoints[jointName].isValid)
+                if (m_handJoints == null || !m_handJoints.IsDefined(jointName) || !m_handJoints[jointName].isValid)
                 {
                     pose = default(JointPose);
                     return false;
