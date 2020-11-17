c290edd279137ea0e0caa802d4829431f81de7f3
ChengNan Yang <chengnan_yang@htc.com>
2020-11-17 16:49:07 +0800

    Fix compile error when ONLY OpenVR XR Plugin installed

--- Assets/HTC.UnityPlugin/VRModule/Modules/SteamVRModule.cs
+++ Assets/HTC.UnityPlugin/VRModule/Modules/SteamVRModule.cs
@@ -1,13 +1,18 @@
 ﻿//========= Copyright 2016-2020, HTC Corporation. All rights reserved. ===========
 
 using HTC.UnityPlugin.Utility;
-#if VIU_STEAMVR && UNITY_STANDALONE
+#if UNITY_STANDALONE
+#if VIU_STEAMVR
 using HTC.UnityPlugin.Vive;
 using HTC.UnityPlugin.Vive.SteamVRExtension;
 using System.Text;
 using UnityEngine;
 using Valve.VR;
 using System;
+#elif VIU_XR_GENERAL_SETTINGS
+using Valve.VR;
+using System;
+#endif
 #if UNITY_2017_2_OR_NEWER
 using UnityEngine.XR;
 #elif UNITY_5_4_OR_NEWER
