08371798977620e69e136506060f9e8f6b4189a1
Naga Chiang <nagachiangdev@gmail.com>
2020-11-26 16:52:28 +0800

    Change HoverEventHandler to ignore ViveProperty

--- Assets/HTC.UnityPlugin/ViveInputUtility/Examples/8.NearFieldHandInteraction/Scripts/HoverEventHandler.cs
+++ Assets/HTC.UnityPlugin/ViveInputUtility/Examples/8.NearFieldHandInteraction/Scripts/HoverEventHandler.cs
@@ -12,65 +12,57 @@ namespace HTC.UnityPlugin.Vive
     public class HoverEventHandler : MonoBehaviour, IColliderEventHoverEnterHandler, IColliderEventHoverExitHandler
     {
         [Serializable] public class HoverEnterEvent : UnityEvent<ColliderHoverEventData> {}
+        [Serializable] public class EveryHoverEnterEvent : UnityEvent<ColliderHoverEventData> {}
         [Serializable] public class HoverStayEvent : UnityEvent<Transform> {}
         [Serializable] public class HoverExitEvent : UnityEvent<ColliderHoverEventData> {}
+        [Serializable] public class EveryHoverExitEvent : UnityEvent<ColliderHoverEventData> {}
 
         public HoverEnterEvent hoverEntered;
+        public EveryHoverEnterEvent everyHoverEntered;
         public HoverStayEvent hoverStaying;
         public HoverExitEvent hoverExited;
+        public EveryHoverExitEvent everyHoverExited;
 
-        private Dictionary<ViveRoleProperty, int> m_stayingRoleCounts = new Dictionary<ViveRoleProperty, int>();
+        private HashSet<ColliderHoverEventData> m_stayingEventDataSet = new HashSet<ColliderHoverEventData>();
         private Transform m_stayingTransform;
 
         public void OnColliderEventHoverEnter(ColliderHoverEventData eventData)
         {
-            ViveColliderEventCaster caster;
-            if (eventData.TryGetEventCaster(out caster))
-            {
-                int count;
-                if (m_stayingRoleCounts.TryGetValue(caster.viveRole, out count))
-                {
-                    m_stayingRoleCounts[caster.viveRole]++;
-                }
-                else
-                {
-                    m_stayingRoleCounts.Add(caster.viveRole, 1);
-                }
+            m_stayingEventDataSet.Add(eventData);
 
-                if (m_stayingRoleCounts[caster.viveRole] == 1)
+            if (m_stayingEventDataSet.Count == 1)
+            {
+                InvokeEnterEvent(eventData);
+
+                ViveColliderEventCaster caster;
+                if (eventData.TryGetEventCaster(out caster))
                 {
-                    InvokeEnterEvent(eventData);
                     m_stayingTransform = caster.transform;
                 }
             }
-            else
-            {
-                InvokeEnterEvent(eventData);
-            }
+
+            InvokeEveryEnterEvent(eventData);
         }
 
         public void OnColliderEventHoverExit(ColliderHoverEventData eventData)
         {
+            m_stayingEventDataSet.Remove(eventData);
+
             ViveColliderEventCaster caster;
             if (eventData.TryGetEventCaster(out caster))
             {
-                Assert.IsTrue(m_stayingRoleCounts.ContainsKey(caster.viveRole), "Exiting role should have entered before.");
-
-                m_stayingRoleCounts[caster.viveRole]--;
-                if (m_stayingRoleCounts[caster.viveRole] == 0)
-                {
-                    InvokeExitEvent(eventData);
-                }
-
                 if (m_stayingTransform == caster.transform)
                 {
                     m_stayingTransform = null;
                 }
             }
-            else
+
+            if (m_stayingEventDataSet.Count == 0)
             {
                 InvokeExitEvent(eventData);
             }
+
+            InvokeEveryExitEvent(eventData);
         }
 
         protected virtual void Update()
@@ -89,6 +81,14 @@ namespace HTC.UnityPlugin.Vive
             }
         }
 
+        private void InvokeEveryEnterEvent(ColliderHoverEventData eventData)
+        {
+            if (everyHoverEntered != null)
+            {
+                everyHoverEntered.Invoke(eventData);
+            }
+        }
+
         private void InvokeStayEvent(Transform transform)
         {
             if (hoverStaying != null)
@@ -102,6 +102,14 @@ namespace HTC.UnityPlugin.Vive
             if (hoverExited != null)
             {
                 hoverExited.Invoke(eventData);
+            }
+        }
+
+        private void InvokeEveryExitEvent(ColliderHoverEventData eventData)
+        {
+            if (everyHoverExited != null)
+            {
+                everyHoverExited.Invoke(eventData);
             }
         }
     }
