baacdbd76387574b16efc0a5bcc430eebb258e3f
lawwong <lawrence_wong@htc.com>
2021-05-13 08:43:37 +0800

    Update version code to v1.13.1.0

--- Assets/HTC.UnityPlugin/ViveInputUtility/Scripts/VIUVersion.cs
+++ Assets/HTC.UnityPlugin/ViveInputUtility/Scripts/VIUVersion.cs
@@ -6,6 +6,6 @@ namespace HTC.UnityPlugin.Vive
 {
     public static class VIUVersion
     {
-        public static readonly Version current = new Version("1.13.0.9");
+        public static readonly Version current = new Version("1.13.1.0");
     }
 }
